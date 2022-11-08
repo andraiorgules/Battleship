11683c3829b48b107579afbdc1adbcd396a864a3
andraiorgules <andraiorgules@gmail.com>
2022-11-08 16:09:26 -0600

    adding files

---
+++ battleship.cpp
@@ -0,0 +1,18 @@
+#include"util.h"
+
+//make 16x16 grid filled with 0s and 1s
+//0 = empty 
+//1 = occupied
+//ships take up 1 space
+//10 ships
+//randomly place ships on grid when game begins
+//write code that scans the grid for ships (gets random coordinate and then removes that coordinate from list to check)
+// checks if that coordinate is a HIT, MISS, or NEAR MISS
+// HIT = ship 
+// NEAR MISS = ship is adjacent in up, down, left, right, or diagonal
+// MISS = no ship + no ship adjacent
+
+int main()
+{
+    foo();
+}
